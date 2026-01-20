9079a42d88b879084792b10ba19a10ec79d19676
Tatiane Marinho <devtatianemarinho@gmail.com>
2026-01-20 15:43:55 -0400

    criando o esboço

--- lib/jogo.dart
+++ lib/jogo.dart
@@ -10,6 +10,38 @@ class Jogo extends StatefulWidget {
 class _JogoState extends State<Jogo> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("Pedra, Papel & Tesoura")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsetsGeometry.only(top: 32, bottom: 16),
+            child: Text(
+              "Escolha do App",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Image.asset("images/padrao.png", height: 100),
+          Padding(
+            padding: EdgeInsetsGeometry.only(top: 32, bottom: 16),
+            child: Text(
+              "Escolha uma opção abaixo",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Image.asset("images/pedra.png", height: 100),
+              Image.asset("images/papel.png", height: 100),
+              Image.asset("images/tesoura.png", height: 100),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
